223910a3576004d8a4a2162c96925b5cd4c93c88
AMOGH PARUVELLI <paruvelli@wisc.edu>
2024-04-25 20:25:24 +0000

    replaced placeholder instances w/ corresponding class

--- App.java
+++ App.java
@@ -4,6 +4,6 @@ public class App {
   public static void main(String[] args) {
     System.out.println("v0.1");
     Frontend.setBackend(new Backend(new DijkstraGraph<String, Double>()));
-    Application.launch(FrontendPlaceholder.class, args);
+    Application.launch(Frontend.class, args);
   }
 }
