aacd7a2ff7af1b4d9d2acde69298b94ccea2bf59
AMOGH PARUVELLI <paruvelli@wisc.edu>
2024-04-25 20:10:34 +0000

    replaced placeholder instances w/ corresponding class

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ import javafx.application.Application;
 public class App {
   public static void main(String[] args) {
     System.out.println("v0.1");
-    FrontendPlaceholder.setBackend(new BackendPlaceholder(new GraphPlaceholder()));
+    Frontend.setBackend(new Backend(new DijkstraGraph()));
     Application.launch(FrontendPlaceholder.class, args);
   }
 }
